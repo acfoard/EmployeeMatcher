8f5b92458f73d6206dea54b8669fb3216202b43c
acfoard <acfoard@gmail.com>
2019-02-08 19:11:14 -0500

    html pages, some node setup

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const path = requre('path');
+const htmlRoute = require(path.join(__dirname, 'app/routing/htmlRoutes.js'))
 const app = express();
+const PORT = 5150;
+
+
+
+
+
+
+app.listen(PORT, function(){
+
+});
